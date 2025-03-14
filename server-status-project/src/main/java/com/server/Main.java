0bbcf02e2a30cefa0baa463fc5cf03a63313e712
aurbanow <aurbanow@pjwstk.edu.pl>
2025-03-14 13:47:12 +0100

    lab 02

--- server-status-project/src/main/java/com/server/Main.java
+++ server-status-project/src/main/java/com/server/Main.java
@@ -33,6 +33,24 @@ public class Main {
 //            return;
 //        }
 
+        /* Testowanie pobierania statystyk serwera */
+
+//        double[] stats = Server.getServerStats(0);
+//        if (stats.length != 3 || stats[0] != 70.5 || stats[1] != 60.2 || stats[2] != 80.1) {
+//            System.out.println("Błąd: Niepoprawne statystyki serwera Alpha");
+//            return;
+//        }
+//        stats = Server.getServerStats(2);
+//        if (stats.length != 3 || stats[0] != 85.0 || stats[1] != 90.5 || stats[2] != 95.2 {
+//            System.out.println("Błąd: Niepoprawne statystyki serwera Alpha");
+//            return;
+//        }
+//        double[] invalidStats = Server.getServerStats(10);
+//        if (invalidStats.length != 0) {
+//            System.out.println("Błąd: Statystyki dla nieistniejącego serwera powinny zwrócić pustą tablicę");
+//            return;
+//        }
+
         /**  Testowanie średniego zużycia zasobów */
 
 //        if (PerformanceMonitor.getAverageUsage(0) != 70.26666666666667) {
